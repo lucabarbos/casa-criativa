66e608d8ba459f411afc8cfec3b54391cfdcbc87
Lucas Barbosa <53865423+lucabarbos@users.noreply.github.com>
2021-01-12 14:59:30 -0300

    casa criativa

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function onOff(){ 
+    document
+        .querySelector("#modal") // procura pelo id footer
+        .classList // acessa lista de classes 
+        .toggle("hide") // tira e coloca a classe hide 
+}
