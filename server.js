6324cc96d17d176da0dc1da785e452e85c3ea855
Lucas Barbosa <53865423+lucabarbos@users.noreply.github.com>
2021-01-13 14:30:31 -0300

    Início back-end

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const server = express()
 //configurar arquivos estáticos (css, script, imagens)
 server.use(express.static("public"))
 
+//configuração do nunjucks 
+const nunjucks = require('nunjucks')
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
 // rota / criada 
 // e capturando requisição do cliente para responder
 server.get("/", function(req, res) {
-    return res.sendFile(__dirname + "/index.html")
+    return res.render("index.html")
 })
 
 server.get("/ideias", function(req, res) {
-    return res.sendFile(__dirname + "/ideias.html")
+    return res.render("ideias.html")
 })
 
 // servidor ligado na porta 3000
