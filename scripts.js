f6bcbffb8c41351fc01ca180d31ed7eb67beab91
Lucas Barbosa <53865423+lucabarbos@users.noreply.github.com>
2021-01-13 10:49:46 -0300

    front-end

--- scripts.js
+++ scripts.js
@@ -2,5 +2,15 @@ function onOff(){
     document
         .querySelector("#modal") // procura pelo id footer
         .classList // acessa lista de classes 
-        .toggle("hide") // tira e coloca a classe hide 
+        .toggle("hide") // tira e coloca a classe hide
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
 }
